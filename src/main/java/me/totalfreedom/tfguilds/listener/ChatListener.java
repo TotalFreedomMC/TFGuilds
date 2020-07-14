40e40c700f41ce3c16b0cf42617771ed413919d5
speed <43330808+speedxx@users.noreply.github.com>
2020-07-14 03:27:47 -0400

    set eventpriority

--- src/main/java/me/totalfreedom/tfguilds/listener/ChatListener.java
+++ src/main/java/me/totalfreedom/tfguilds/listener/ChatListener.java
@@ -6,12 +6,13 @@ import me.totalfreedom.tfguilds.util.GUtil;
 import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
 
 public class ChatListener implements Listener
 {
-    @EventHandler
+    @EventHandler(priority = EventPriority.HIGHEST)
     public void onPlayerChat(AsyncPlayerChatEvent e)
     {
         Player player = e.getPlayer();
